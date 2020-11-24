e411cbd949e6af896ec180e6dfcfcb9ce82e6d47
Ubuntu <ubuntu@ip-172-31-14-150.eu-north-1.compute.internal>
2020-11-24 13:41:56 +0000

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { initListings, addListing, deleteListing } from './redux/listingsReducer';
+
+const SubmitListing = () => {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const dispatch = useDispatch();
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    dispatch(addListing(name, age));
+  };
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={({ target}) => setName(target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Age"
+        onChange={({ target}) => setAge(target.value)}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 function App() {
+  const dispatch = useDispatch();
+  const listings = useSelector((state) => state.listings)
+  
+  useEffect(() => {
+    dispatch(initListings());
+  }, [dispatch]);
+  
+  const removeListing = (listingName) => {
+    dispatch(deleteListing(listingName));
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {listings.map((listing) => (
+        <p>{listing.name}, {listing.age} | <span onClick={() => removeListing(listing.name)}>(X)</span> </p>
+      ))}
+      <br />
+      <SubmitListing />
     </div>
   );
-}
+};
 
 export default App;
